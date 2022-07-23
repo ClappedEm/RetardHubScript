87a71d9880925e5ad2ba4136888c40809cf89ebe
ClappedEm <68822716+ClappedEm@users.noreply.github.com>
2022-07-22 22:08:07 -0700

    DEV STAGE 1

---
+++ RetardLoader.lua
@@ -0,0 +1,16 @@
+repeat wait() until game:IsLoaded()
+
+-- [🍀x5 CONQ HAKI] A 0ne Piece Game
+if (game.PlaceId == 8396586868 ) then
+    -- Sea 1
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/AopgScriptLoader.lua"))()
+    
+elseif  (game.PlaceId == 9432106399 ) then 
+    -- Sea 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/AopgScriptLoader2.lua"))()
+
+
+    -- [RELEASE🌊⚡👺] Project Slayers
+elseif (game.PlaceId == 6152116144) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/ProjectSlayers.lua"))()
+end
