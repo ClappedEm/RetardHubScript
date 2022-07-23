679374e0b22d602177d112a93f2d88d9b5ab44cf
ClappedEm <68822716+ClappedEm@users.noreply.github.com>
2022-07-22 22:40:37 -0700

    DEV STAGE 2.01

--- RetardLoader.lua
+++ RetardLoader.lua
@@ -2,15 +2,18 @@ repeat wait() until game:IsLoaded()
 
 -- [🍀x5 CONQ HAKI] A 0ne Piece Game
 if (game.PlaceId == 8396586868 ) then
-    -- Sea 1
+    -- Sea 1 and Sea 2
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/AopgScriptLoader.lua"))()
     
-elseif  (game.PlaceId == 9432106399 ) then 
-    -- Sea 2
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/AopgScriptLoader2.lua"))()
-
-
-    -- [RELEASE🌊⚡👺] Project Slayers
+ 
 elseif (game.PlaceId == 6152116144) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/Simple-Script-Loader/main/ProjectSlayers.lua"))()
+    -- [RELEASE🌊⚡👺] Project Slayers
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ClappedEm/RetardHubScript/master/Scripts/Games/ProjectSlayers/ProjectSlayers.lua"))()
+
+else
+print("Unsupported Game")
+wait(10)
+print("Executing Universal Script...")
+game.Players.LocalPlayer:Kick()
+
 end
